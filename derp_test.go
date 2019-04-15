7020da7b00e21f51ac85295fa16b4eb201afc0e4
Ben Pate <ben@JARVIS.local>
2019-04-15 13:57:54 -0600

    Improved test cases

--- derp_test.go
+++ derp_test.go
@@ -3,18 +3,28 @@ package derp
 import (
 	"testing"
 
-	"github.com/benpate/derp"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestDerp tests basic derp functions (separate from features of a specific reporter)
 func TestDerp(t *testing.T) {
 
-	if err := derp.New(); err != nil {
+	// Create an inner error
+	innerError := New("TestDerp", "Inner Error: Not Found", CodeNotFound, nil, "detail1", "detail2", "detail3")
 
-		if err.NotFound() {
+	assert.Equal(t, innerError.Location, "TestDerp")
+	assert.Equal(t, innerError.Message, "Inner Error: Not Found")
+	assert.Equal(t, innerError.Code, 404)
+	assert.Equal(t, innerError.Details[0], "detail1")
+	assert.Equal(t, innerError.Details[1], "detail2")
+	assert.Equal(t, innerError.Details[2], "detail3")
 
-		}
-	}
+	// Create an outer error
+	outerError := New("TestDerp", "OuterError", 0, innerError, "other details here")
 
-	derp.New().Report()
+	assert.Equal(t, outerError.Location, "TestDerp")
+	assert.Equal(t, outerError.Message, "OuterError")
+	assert.Equal(t, outerError.Code, 404) // This is still 404 because we've let the inner error code bubble up
+	assert.NotNil(t, outerError.InnerError)
+	assert.Equal(t, outerError.Details[0], "other details here")
 }

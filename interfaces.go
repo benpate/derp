873b72b47205b815add316ffce671409085b9319
Ben Pate <ben@pate.org>
2026-06-13 09:17:55 -0600

    Comments

--- interfaces.go
+++ interfaces.go
@@ -4,40 +4,40 @@ import "time"
 
 // DetailsGetter interface wraps the GetDetails method, which returns a list of details about the error
 type DetailsGetter interface {
-	// Details returns a list of details about the error.
+	// GetDetails returns a list of details about the error.
 	GetDetails() []any
 }
 
 // ErrorCodeGetter interface wraps the GetErrorCode method, which returns a numeric, application-specific code that references this error
 type ErrorCodeGetter interface {
 
-	// ErrorCode returns a numeric, application-specific code that references this error.
+	// GetErrorCode returns a numeric, application-specific code that references this error.
 	// HTTP status codes are recommended, but not required
 	GetErrorCode() int
 }
 
 // LocationGetter interface wraps the GetLocation method, which returns the location of the error
 type LocationGetter interface {
-	// Location returns the location of the error in the source code.
+	// GetLocation returns the location of the error in the source code.
 	GetLocation() string
 }
 
 // MessageGetter interface wraps the GetMessage method, which returns a human-friendly string representation of the error
 type MessageGetter interface {
 
-	// Message returns a human-friendly string representation of the error.
+	// GetMessage returns a human-friendly string representation of the error.
 	GetMessage() string
 }
 
 // RetryAfterGetter interface wraps the GetRetryAfter method, which returns the number of seconds to wait before retrying the operation that caused this error
 type RetryAfterGetter interface {
-	// RetryAfter returns the number of seconds to wait before retrying the operation that caused this error.
+	// GetRetryAfter returns the number of seconds to wait before retrying the operation that caused this error.
 	GetRetryAfter() time.Duration
 }
 
 // URLGetter interface wraps the GetURL method, which returns a URL to a web page with more information about this error
 type URLGetter interface {
-	// URL returns a URL to a web page with more information about this error.
+	// GetURL returns a URL to a web page with more information about this error.
 	GetURL() string
 }
 
